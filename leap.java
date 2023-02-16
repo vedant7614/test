6ffac356d685f254aba31e31f323556244aeb8f5
vedant7614 <74951656+vedant7614@users.noreply.github.com>
2023-02-16 23:26:05 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,10 @@
+class leap{
+	public static void main(String[] args) {
+		int year = 2016;
+		if (year%4==0)
+			System.out.println("The entered year is a leap year");
+		else
+			System.out.println("The entered year is not leap year");
+
+	}
+}
